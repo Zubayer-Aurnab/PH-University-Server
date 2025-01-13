bdb13b2d52e6db5d3ef720e9c11697f255957f70
Aurnab <zubayeraurnab@gmail.com>
2025-01-13 22:41:09 +0600

    inital setup completed

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+// eslint.config.mjs
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.browser } },
+  {
+    rules: {
+      eqeqeq: "off",
+      "no-unused-vars": "error",
+      "prefer-const": ["error", { ignoreReadBeforeAssign: true }],
+      "no-unused-expressions": "error",
+      "no-console": "warn",
+      "no-undef": "error",
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: [".node_modules/*", "dist/*"]
+  },
+];
